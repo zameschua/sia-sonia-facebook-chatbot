958564e1f2f533b70cd03ebf7aa7dc221a43483b
Zames Chua <zames.chua@gmail.com>
2016-10-06 13:17:26 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const http = require('http')
+const Bot = require('messenger-bot')
+const request = require('request');
+
+
+var bot = new Bot({
+  token: 'EAADNE5iTX2kBAMUkSZAOgh27tQPlElKHJcC2pMM5tsiTbnBfHyoV0yQqWEAo9mBj6VNS9ybFWs5CRq8MBu6SjAIIHYZCtabHGM7qpyHIQoZBT2FXBFKcLmV1qiT6HjyUpmEm6GCflaTbrfTE0DJFOPx1BLg57b6rgQXVDS7ZBQZDZD',
+  verify: 'sia-app-challenge-bot'
+})
+
+
+var tempDB = {
+	1292592927427179: {name: "Zames", flight: "SQKII10"}
+}
+
+
+
+bot.on('error', (err) => {
+  console.log(err.message)
+})
+
+
+bot.on('message', (payload, reply) => {
+	var fbid = payload.sender.id;
+    bot.getProfile(fbid, (err, profile) => {
+		if (err) throw err;
+	    if (fbid in tempDB) {
+	    	var lat = 1.3644202;
+			var long = 103.99153079999996;
+	    	var mapJSON = {	    
+	    		"attachment": {
+			        "type": "template",
+			        "payload": {
+			            "template_type": "generic",
+			            "elements": {
+			                "element": {
+			                    "title": "Follow the map to get to Belt 16!", //Change to belt number??
+			                    "image_url": "https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center="+lat+","+long+"&zoom=15&markers="+lat+","+long,
+			                    "item_url": "http:\/\/maps.apple.com\/maps?q="+lat+","+long+"&z=15"
+			                }
+			            }
+			        }
+	    		}
+	    	}
+	    	reply(mapJSON, function(err, info) {
+	    		if (err) {
+	    			console.log(err);
+	    		};
+	    	})
+	    	
+	    } else {
+	    	var dbData = {
+	    		fbid: {
+	    			name: profile.first_name,
+	    			flight: "SQKII10", //CHECK IF MESSAGE IS FLIGHT NUMBER
+	    		}
+	    	}
+	    	//Put data into database
+	    	//Give flight info
+	    	//Stack the alert messages into queue
+	    	console.log(profile);
+	    }
+	})
+
+
+
+
+
+
+
+//    var text = payload.message.text;
+
+//    console.log(payload);
+
+
+
+//    reply({ text }, (err) => {
+//      if (err) throw err
+
+//      console.log(`Echoed back to ${profile.first_name}               ${profile.last_name}: ${text}`)
+//    })
+	console.log(tempDB);
+})
+
+http.createServer(bot.middleware()).listen(8080)
+console.log('Echo bot server running at port 8080.')
+
+
+
+
+
+var fakeJson = {"code":200,"customers":[{"flightInfo":{"qrCodeBinary":"longassshithere","departureGate":"","departureAirport":"Singapore Changi","cabin":"Economy","eTicketNumber":"618241177233101","seatNumber":"047D","bookingReference":"248LJV","aircraftType":"Boeing 777-300ER","departureDate":"13 Jan","scheduledArrivalTime":"07:20AM","airlineUse":"001","departureTerminal":"T3","flightDeck":"","offPoint":"London","fullName":"TEST MR","boardPoint":"Singapore","isA380":false,"boardingDateTime":"12:15AM 13 Jan","membershipNo":"","boardPointCode":"SIN","flightNumber":"SQ306","boardingZone":
+"5","scheduledDepartureTime":"01:15AM","offPointCode":"LHR","arrivalAirport":"London Heathrow","did":"2301D78000006503","loungeText":"","operatingAirline":"Singapore Airlines"},"uci":"2301D78000006CA2"}],"message":""}
+
+
+/*
+	    bot.sendMessage(payload.sender.id, reply, function(err, info){
+	    	if (err){
+	    		console.log(err);
+	    	} else {
+	    		console.log(info);
+	    	}
+	    })
+var link = "https://graph.facebook.com/v2.6/me/messages?access_token=EAADNE5iTX2kBAMUkSZAOgh27tQPlElKHJcC2pMM5tsiTbnBfHyoV0yQqWEAo9mBj6VNS9ybFWs5CRq8MBu6SjAIIHYZCtabHGM7qpyHIQoZBT2FXBFKcLmV1qiT6HjyUpmEm6GCflaTbrfTE0DJFOPx1BLg57b6rgQXVDS7ZBQZDZD"
+*/
